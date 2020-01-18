c989dd9377a42e055827e4ff0b7445eab86a0c3e
wx-chevalier <wxchevalier925@gmail.com>
2020-01-18 15:50:24 +0800

    feat: update Home

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 本仓库仅存放有 [xCompass](https://github.com/wx-chevalier/xCompass) 等项目的编译后文件。
 
-# Motivation & Credits
+# About
+
+## Motivation & Credits
 
 - https://github.com/PavelDoGreat/WebGL-Fluid-Simulation
--- books/index.js
+++ books/index.js
@@ -1,18 +1,151 @@
-let bookContainer = document.querySelector('.search');
-let searchBooks = document.getElementById('search-box');
-const getBooks = async book => {
-  const response = await fetch(
-    `https://www.googleapis.com/books/v1/volumes?q=${book}`
-  );
-  const data = await response.json();
-  return data;
+let bookContainer = document.querySelector(".search");
+let searchBooks = document.getElementById("search-box");
+
+/** @start 添加元素 */
+[
+  { id: "resource", title: "参考资料、书籍、视频、开源项目", icon: "" },
+  { id: "pl", title: "编程语言", icon: "" },
+  { id: "se", title: "数据结构、算法、软件架构", icon: "" },
+  { id: "fe", title: "Web、大前端、可视化", icon: "" },
+  { id: "be", title: "服务端、微服务、云原生", icon: "" },
+  { id: "infras", title: "分布式系统、数据库、虚拟化", icon: "" },
+  { id: "ai", title: "人工智能、机器学习、深度学习", icon: "" },
+  { id: "product", title: "产品、运营、创业", icon: "" }
+].forEach(({ id, title, icon }) => {
+  document.addEventListener("DOMContentLoaded", () => {
+    drawChartBook(id);
+  });
+
+  document.querySelector("#foryou").innerHTML =
+    document.querySelector("#foryou").innerHTML +
+    `
+      <section id=${id} class="results">
+        <div class="flex">
+          <h1 class="section-title">${title}</h1>
+          <div>
+            <button id="${id}-prev" class="pagination prev" onclick="prev('${id}')">◀</button>
+            <button id="${id}-next" class="pagination next" onclick="next('${id}')">▶</button>
+          </div>
+        </div>
+        <div class="list-book ${id} categories">
+          <div class='prompt'>
+            <div class="loader"></div>
+          </div>
+        </div>
+        <div class="fade left"></div>
+        <div class="fade right"></div>
+      </section>
+  `;
+});
+
+/** @end 添加元素 */
+
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const getCover = title => {
+  return `https://orly-appstore.herokuapp.com/generate?title=${title}&top_text=Just%20Coder&author=wx-chevalier&image_code=${randomIntFromInterval(
+    1,
+    40
+  )}&theme=${randomIntFromInterval(
+    0,
+    16
+  )}&guide_text=&guide_text_placement=bottom_right`;
 };
+
+/** 执行书籍抓取 */
+const getBooks = async book => {
+  let data = [];
+
+  switch (book) {
+    case "resource": {
+      data = [
+        {
+          volumeInfo: {
+            title: "CS 资料集锦",
+            previewLink: "https://ng-tech.icu/Awesome-Lists",
+            imageLinks: {
+              thumbnail: getCover("Awesome Lists")
+            },
+            categories: ["Awesome Lists"]
+          }
+        }
+      ];
+      break;
+    }
+    case "pl": {
+      data = [
+        {
+          volumeInfo: {
+            title: "Java 实战",
+            previewLink: "https://ng-tech.icu/Java-Series/#/",
+            imageLinks: {
+              thumbnail: getCover("Java Series")
+            },
+            categories: ["Java", "Spring"]
+          }
+        },
+        {
+          volumeInfo: {
+            title: "JavaScript 实战",
+            previewLink: "https://ng-tech.icu/JavaScript-Series/#/",
+            imageLinks: {
+              thumbnail: getCover("JavaScript Series")
+            },
+            categories: ["JavaScript"]
+          }
+        },
+        {
+          volumeInfo: {
+            title: "Python 实战",
+            previewLink: "https://ng-tech.icu/ProgrammingLanguage-Series/#/",
+            imageLinks: {
+              thumbnail: getCover("Python Series")
+            },
+            categories: ["Python"]
+          }
+        },
+        {
+          volumeInfo: {
+            title: "Go 实战",
+            previewLink: "https://ng-tech.icu/Go-Series/#/",
+            imageLinks: {
+              thumbnail: getCover("Go Series")
+            },
+            categories: ["Go"]
+          }
+        },
+        {
+          volumeInfo: {
+            title: "Rust 实战",
+            previewLink: "https://ng-tech.icu/ProgrammingLanguage-Series/#/",
+            imageLinks: {
+              thumbnail: getCover("Rust Series")
+            },
+            categories: ["Rust"]
+          }
+        }
+      ];
+      break;
+    }
+    case "se": {
+      data = [];
+      break;
+    }
+  }
+  data.forEach(d => {
+    d.volumeInfo.authors = ["月熊"];
+  });
+
+  return { totalItems: data.length, items: data };
+};
+
 const drawChartBook = async (subject, startIndex = 0) => {
   let cbookContainer = document.querySelector(`.${subject}`);
   cbookContainer.innerHTML = `<div class='prompt'><div class="loader"></div></div>`;
-  const cdata = await getBooks(
-    `subject:${subject}&startIndex=${startIndex}&maxResults=6`
-  );
+  const cdata = await getBooks(subject);
   if (cdata.error) {
     cbookContainer.innerHTML = `<div class='prompt'>ツ Limit exceeded! Try after some time</div>`;
   } else if (cdata.totalItems == 0) {
@@ -28,22 +161,22 @@ const drawChartBook = async (subject, startIndex = 0) => {
           getRandomColor() +
           `, rgba(0, 0, 0, 0));'><a href='${volumeInfo.previewLink}' target='_blank'><img class='thumbnail' src='` +
           (volumeInfo.imageLinks.thumbnail === undefined
-            ? 'icons/logo.svg'
-            : volumeInfo.imageLinks.thumbnail.replace('http://', 'https://')) +
+            ? "icons/logo.svg"
+            : volumeInfo.imageLinks.thumbnail.replace("http://", "https://")) +
           `' alt='cover'></a><div class='book-info'><h3 class='book-title'><a href='${volumeInfo.previewLink}' target='_blank'>${volumeInfo.title}</a></h3><div class='book-authors' onclick='updateFilter(this,"author");'>${volumeInfo.authors}</div><div class='info' onclick='updateFilter(this,"subject");' style='background-color: ` +
           getRandomColor() +
           `;'>` +
           (volumeInfo.categories === undefined
-            ? 'Others'
+            ? "Others"
             : volumeInfo.categories) +
           `</div></div></div>`
       )
-      .join('');
+      .join("");
   }
 };
 const drawListBook = async () => {
-  if (searchBooks.value != '') {
-    bookContainer.style.display = 'flex';
+  if (searchBooks.value != "") {
+    bookContainer.style.display = "flex";
     bookContainer.innerHTML = `<div class='prompt'><div class="loader"></div></div>`;
     const data = await getBooks(`${searchBooks.value}&maxResults=6`);
     if (data.error) {
@@ -60,36 +193,36 @@ const drawListBook = async () => {
             getRandomColor() +
             `, rgba(0, 0, 0, 0));'><a href='${volumeInfo.previewLink}' target='_blank'><img class='thumbnail' src='` +
             (volumeInfo.imageLinks.thumbnail === undefined
-              ? 'icons/logo.svg'
+              ? "icons/logo.svg"
               : volumeInfo.imageLinks.thumbnail.replace(
-                  'http://',
-                  'https://'
+                  "http://",
+                  "https://"
                 )) +
             `' alt='cover'></a><div class='book-info'><h3 class='book-title'><a href='${volumeInfo.previewLink}' target='_blank'>${volumeInfo.title}</a></h3><div class='book-authors' onclick='updateFilter(this,"author");'>${volumeInfo.authors}</div><div class='info' onclick='updateFilter(this,"subject");' style='background-color: ` +
             getRandomColor() +
             `;'>` +
             (volumeInfo.categories === undefined
-              ? 'Others'
+              ? "Others"
               : volumeInfo.categories) +
             `</div></div></div>`
         )
-        .join('');
+        .join("");
     }
   } else {
-    bookContainer.style.display = 'none';
+    bookContainer.style.display = "none";
   }
 };
 const updateFilter = ({ innerHTML }, f) => {
-  document.getElementById('main').scrollIntoView({
-    behavior: 'smooth'
+  document.getElementById("main").scrollIntoView({
+    behavior: "smooth"
   });
   let m;
   switch (f) {
-    case 'author':
-      m = 'inauthor:';
+    case "author":
+      m = "inauthor:";
       break;
-    case 'subject':
-      m = 'subject:';
+    case "subject":
+      m = "subject:";
       break;
   }
   searchBooks.value = m + innerHTML;
@@ -98,19 +231,10 @@ const updateFilter = ({ innerHTML }, f) => {
 const debounce = (fn, time, to = 0) => {
   to ? clearTimeout(to) : (to = setTimeout(drawListBook, time));
 };
-searchBooks.addEventListener('input', () => debounce(drawListBook, 1000));
-document.addEventListener('DOMContentLoaded', () => {
-  drawChartBook('love');
-  drawChartBook('feminism');
-  drawChartBook('inspirational');
-  drawChartBook('authors');
-  drawChartBook('fiction');
-  drawChartBook('poetry');
-  drawChartBook('fantasy');
-  drawChartBook('romance');
-});
-let mainNavLinks = document.querySelectorAll('.scrolltoview');
-window.addEventListener('scroll', event => {
+searchBooks.addEventListener("input", () => debounce(drawListBook, 1000));
+
+let mainNavLinks = document.querySelectorAll(".scrolltoview");
+window.addEventListener("scroll", event => {
   let fromTop = window.scrollY + 64;
   mainNavLinks.forEach(({ hash, classList }) => {
     let section = document.querySelector(hash);
@@ -118,9 +242,9 @@ window.addEventListener('scroll', event => {
       section.offsetTop <= fromTop &&
       section.offsetTop + section.offsetHeight > fromTop
     ) {
-      classList.add('current');
+      classList.add("current");
     } else {
-      classList.remove('current');
+      classList.remove("current");
     }
   });
 });
@@ -129,45 +253,45 @@ const getRandomColor = () =>
 const toggleSwitch = document.querySelector(
   '.theme-switch input[type="checkbox"]'
 );
-if (localStorage.getItem('marcdownTheme') == 'dark') {
-  document.documentElement.setAttribute('data-theme', 'dark');
+if (localStorage.getItem("marcdownTheme") == "dark") {
+  document.documentElement.setAttribute("data-theme", "dark");
   document
-    .querySelector('meta[name=theme-color]')
-    .setAttribute('content', '#090b28');
+    .querySelector("meta[name=theme-color]")
+    .setAttribute("content", "#090b28");
   toggleSwitch.checked = true;
-  localStorage.setItem('marcdownTheme', 'dark');
+  localStorage.setItem("marcdownTheme", "dark");
 } else {
-  document.documentElement.setAttribute('data-theme', 'light');
+  document.documentElement.setAttribute("data-theme", "light");
   document
-    .querySelector('meta[name=theme-color]')
-    .setAttribute('content', '#ffffff');
+    .querySelector("meta[name=theme-color]")
+    .setAttribute("content", "#ffffff");
   toggleSwitch.checked = false;
-  localStorage.setItem('marcdownTheme', 'light');
+  localStorage.setItem("marcdownTheme", "light");
 }
 const switchTheme = ({ target }) => {
   if (target.checked) {
-    document.documentElement.setAttribute('data-theme', 'dark');
+    document.documentElement.setAttribute("data-theme", "dark");
     document
-      .querySelector('meta[name=theme-color]')
-      .setAttribute('content', '#090b28');
-    localStorage.setItem('marcdownTheme', 'dark');
+      .querySelector("meta[name=theme-color]")
+      .setAttribute("content", "#090b28");
+    localStorage.setItem("marcdownTheme", "dark");
   } else {
-    document.documentElement.setAttribute('data-theme', 'light');
+    document.documentElement.setAttribute("data-theme", "light");
     document
-      .querySelector('meta[name=theme-color]')
-      .setAttribute('content', '#ffffff');
-    localStorage.setItem('marcdownTheme', 'light');
+      .querySelector("meta[name=theme-color]")
+      .setAttribute("content", "#ffffff");
+    localStorage.setItem("marcdownTheme", "light");
   }
 };
-toggleSwitch.addEventListener('change', switchTheme, false);
+toggleSwitch.addEventListener("change", switchTheme, false);
 let startIndex = 0;
 const next = subject => {
   startIndex += 6;
   if (startIndex >= 0) {
-    document.getElementById(`${subject}-prev`).style.display = 'inline-flex';
+    document.getElementById(`${subject}-prev`).style.display = "inline-flex";
     drawChartBook(subject, startIndex);
   } else {
-    document.getElementById(`${subject}-prev`).style.display = 'none';
+    document.getElementById(`${subject}-prev`).style.display = "none";
   }
 };
 const prev = subject => {
@@ -175,57 +299,46 @@ const prev = subject => {
   if (startIndex <= 0) {
     startIndex = 0;
     drawChartBook(subject, startIndex);
-    document.getElementById(`${subject}-prev`).style.display = 'none';
+    document.getElementById(`${subject}-prev`).style.display = "none";
   } else {
-    document.getElementById(`${subject}-prev`).style.display = 'inline-flex';
+    document.getElementById(`${subject}-prev`).style.display = "inline-flex";
     drawChartBook(subject, startIndex);
   }
 };
-const modal = document.querySelector('.modal');
-const trigger = document.querySelector('.trigger');
-const closeButton = document.querySelector('.close-button');
-const toggleModal = () => modal.classList.toggle('show-modal');
-const windowOnClick = ({ target }) => {
-  if (target === modal) {
-    toggleModal();
-  }
-};
-trigger.addEventListener('click', toggleModal);
-closeButton.addEventListener('click', toggleModal);
-window.addEventListener('click', windowOnClick);
-let pwaInstalled = localStorage.getItem('pwaInstalled') == 'yes';
-if (window.matchMedia('(display-mode: standalone)').matches) {
-  localStorage.setItem('pwaInstalled', 'yes');
+
+let pwaInstalled = localStorage.getItem("pwaInstalled") == "yes";
+if (window.matchMedia("(display-mode: standalone)").matches) {
+  localStorage.setItem("pwaInstalled", "yes");
   pwaInstalled = true;
 }
 if (window.navigator.standalone === true) {
-  localStorage.setItem('pwaInstalled', 'yes');
+  localStorage.setItem("pwaInstalled", "yes");
   pwaInstalled = true;
 }
 if (pwaInstalled) {
-  document.getElementById('installPWA').style.display = 'none';
+  document.getElementById("installPWA").style.display = "none";
 } else {
-  document.getElementById('installPWA').style.display = 'inline-flex';
+  document.getElementById("installPWA").style.display = "inline-flex";
 }
 let deferredPrompt = null;
-window.addEventListener('beforeinstallprompt', e => {
+window.addEventListener("beforeinstallprompt", e => {
   deferredPrompt = e;
 });
 async function installPWA() {
   if (deferredPrompt) {
     deferredPrompt.prompt();
     deferredPrompt.userChoice.then(({ outcome }) => {
-      if (outcome === 'accepted') {
-        console.log('Your PWA has been installed');
+      if (outcome === "accepted") {
+        console.log("Your PWA has been installed");
       } else {
-        console.log('User chose to not install your PWA');
+        console.log("User chose to not install your PWA");
       }
       deferredPrompt = null;
     });
   }
 }
-window.addEventListener('appinstalled', evt => {
-  localStorage.setItem('pwaInstalled', 'yes');
+window.addEventListener("appinstalled", evt => {
+  localStorage.setItem("pwaInstalled", "yes");
   pwaInstalled = true;
-  document.getElementById('installPWA').style.display = 'none';
+  document.getElementById("installPWA").style.display = "none";
 });
